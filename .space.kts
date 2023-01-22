dd84b7b787b751d3becd6e625071b80eabb822c1
Michael Hofmann <>
2023-01-22 01:49:45 +0100

    Add Qodana job to .space.kts

--- .space.kts
+++ .space.kts
@@ -4,6 +4,30 @@
 * For more info, see https://www.jetbrains.com/help/space/automation.html
 */
 
+job("Qodana") {
+    startOn {
+        gitPush {
+            branchFilter {
+                +"refs/heads/feature"
+            }
+        }
+        codeReviewOpened{}
+    }
+    container("jetbrains/qodana-<linter>") {
+        env["QODANA_TOKEN"] = Secrets("qodana-token")
+        shellScript {
+            content = """
+            QODANA_REMOTE_URL="ssh://git@git.${'$'}JB_SPACE_API_URL/${'$'}JB_SPACE_PROJECT_KEY/${'$'}JB_SPACE_GIT_REPOSITORY_NAME.git" \
+            QODANA_BRANCH=${'$'}JB_SPACE_GIT_BRANCH \
+            QODANA_REVISION=${'$'}JB_SPACE_GIT_REVISION \
+            qodana
+            --fail-threshold <number>
+            --profile-name <profile-name>
+            """.trimIndent()
+        }
+    }
+}
+
 job("Build and run  tests") {
     requirements { workerType = WorkerTypes.SPACE_CLOUD_UBUNTU_LTS_LARGE }
 
