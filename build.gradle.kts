4558b182e53562572cddee539d8bb0365bb51911
Michael Hofmann <>
2023-01-28 15:44:16 +0100

    Add redundant gradle build tools version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 buildscript {
     dependencies {
-        classpath("com.android.tools.build:gradle:3.4.0")
         classpath("com.google.gms:google-services:4.3.15")
         classpath("com.google.firebase:firebase-appdistribution-gradle:3.2.0")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.2")
