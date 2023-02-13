e1e99ac7724235efd5f17522e466ca75dfe78ecd
Michael Hofmann <>
2023-02-13 18:35:25 +0100

    Update app icon

--- app/src/main/java/app/tinygiants/getalife/ui/theme/Type.kt
+++ app/src/main/java/app/tinygiants/getalife/ui/theme/Type.kt
@@ -1,34 +1,145 @@
 package app.tinygiants.getalife.ui.theme
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.material3.Typography
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import app.tinygiants.getalife.R
 
-// Set of Material typography styles to start with
+val nunitoFontFamily = FontFamily(
+    Font(R.font.nunito),
+    Font(R.font.nunito_italic, style = FontStyle.Italic)
+)
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    displayLarge = TextStyle(
+        fontFamily = nunitoFontFamily,
+        fontWeight = FontWeight.Light,
+        fontSize = 101.sp,
+        lineHeight = 64.sp,
+        letterSpacing = (-1.5).sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = nunitoFontFamily,
+        fontWeight = FontWeight.Light,
+        fontSize = 45.sp,
+        lineHeight = 52.sp,
+        letterSpacing = (-0.5).sp
+    ),
+    displaySmall = TextStyle(
+        fontFamily = nunitoFontFamily,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
+        fontSize = 36.sp,
+        lineHeight = 44.sp,
         letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+    headlineLarge = TextStyle(
+        fontFamily = nunitoFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 32.sp,
+        lineHeight = 40.sp,
+        letterSpacing = 0.25.sp
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = nunitoFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 28.sp,
+        lineHeight = 36.sp,
+        letterSpacing = 0.sp
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = nunitoFontFamily,
         fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
+        fontSize = 24.sp,
+        lineHeight = 32.sp,
+        letterSpacing = 0.15.sp
+    ),
+    labelLarge = TextStyle(
+        fontFamily = nunitoFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.1.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = nunitoFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 12.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = nunitoFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = nunitoFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = nunitoFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 17.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.25.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = nunitoFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.4.sp
     )
-    */
 )
+
+@Preview(widthDp = 600)
+@Composable
+fun TypographyPreview() {
+    GetALifeTheme {
+        Surface {
+            Column(
+                modifier = Modifier.padding(8.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                Text("displayLarge", style = MaterialTheme.typography.displayLarge)
+                Text("displayMedium", style = MaterialTheme.typography.displayMedium)
+                Text("displaySmall", style = MaterialTheme.typography.displaySmall)
+
+                Text("headlineLarge", style = MaterialTheme.typography.headlineLarge)
+                Text("headlineMedium", style = MaterialTheme.typography.headlineMedium)
+                Text("headlineSmall", style = MaterialTheme.typography.headlineSmall)
+
+                Text("titleLarge", style = MaterialTheme.typography.titleLarge)
+                Text("titleMedium", style = MaterialTheme.typography.titleMedium)
+                Text("titleSmall", style = MaterialTheme.typography.titleSmall)
+
+                Text("labelLarge", style = MaterialTheme.typography.labelLarge)
+                Text("labelMedium", style = MaterialTheme.typography.labelMedium)
+                Text("labelSmall", style = MaterialTheme.typography.labelSmall)
+
+                Text("bodyLarge", style = MaterialTheme.typography.bodyLarge)
+                Text("bodyMedium", style = MaterialTheme.typography.bodyMedium)
+                Text("bodySmall", style = MaterialTheme.typography.bodySmall)
+            }
+        }
+    }
+}
