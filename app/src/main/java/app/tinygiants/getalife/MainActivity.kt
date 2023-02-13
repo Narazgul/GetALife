73065339d761c00827db7935a66ae2101aff97f6
Michael Hofmann <>
2023-02-13 18:38:32 +0100

    Add test screen to MainActivity.kt

--- app/src/main/java/app/tinygiants/getalife/MainActivity.kt
+++ app/src/main/java/app/tinygiants/getalife/MainActivity.kt
@@ -1,13 +1,16 @@
 package app.tinygiants.getalife
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import app.tinygiants.getalife.ui.theme.GetALifeTheme
@@ -17,12 +20,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             GetALifeTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    HomeScreen()
                 }
             }
         }
@@ -30,14 +32,19 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun HomeScreen(modifier: Modifier = Modifier) {
+    Box(modifier = modifier, contentAlignment = Alignment.Center) {
+        Text(text = "Test", style = MaterialTheme.typography.displayLarge)
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(name = "Light", widthDp = 400, heightDp = 400)
+@Preview(name = "Dark", widthDp = 400, heightDp = 400, uiMode = UI_MODE_NIGHT_YES)
 @Composable
-fun DefaultPreview() {
+fun HomeScreenPreview() {
     GetALifeTheme {
-        Greeting("Android")
+        Surface {
+            HomeScreen()
+        }
     }
 }
