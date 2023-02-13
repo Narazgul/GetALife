484e235912ba3c79fbd0df6e5a7d74cbc167a615
Michael Hofmann <>
2023-02-13 18:36:22 +0100

    Add Shape file

---
+++ app/src/main/java/app/tinygiants/getalife/ui/theme/Shape.kt
@@ -0,0 +1,12 @@
+package app.tinygiants.getalife.ui.theme
+
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shape = Shapes(
+    small = CutCornerShape(topStart = 8.dp),
+    medium = CutCornerShape(topStart = 24.dp),
+    large = RoundedCornerShape(8.dp)
+)
