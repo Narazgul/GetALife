1c7bb2302049c5dc7a9c797c117118cfafc26dd9
Michael Hofmann <>
2023-02-24 16:08:23 +0100

    Update Gradle dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,11 +79,11 @@ android {
 dependencies {
     // core
     implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.0-beta01")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.0-rc01")
     implementation("androidx.activity:activity-compose:1.6.1")
-    implementation("androidx.compose.ui:ui:1.4.0-alpha05")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.4.0-alpha05")
-    implementation("androidx.compose.material3:material3:1.1.0-alpha05")
+    implementation("androidx.compose.ui:ui:1.4.0-beta02")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.4.0-beta02")
+    implementation("androidx.compose.material3:material3:1.1.0-alpha07")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
 
     // firebase
@@ -100,9 +100,9 @@ dependencies {
     // testing
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.4.0-alpha05")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.4.0-beta02")
 
     // debug build
-    debugImplementation("androidx.compose.ui:ui-tooling:1.4.0-alpha05")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.4.0-alpha05")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.4.0-beta02")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.4.0-beta02")
 }
