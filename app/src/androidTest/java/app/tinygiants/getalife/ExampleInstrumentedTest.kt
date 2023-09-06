e8f4376f4f399f9383d1c5963d51a5bef911ff3a
Michael Hofmann <michael.hofmann@stock3.com>
2023-09-06 14:47:49 +0200

    Use ksp instead of kapt

--- app/src/androidTest/java/app/tinygiants/getalife/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/app/tinygiants/getalife/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("app.tinygiants.getalife", appContext.packageName)
+        assertEquals("app.tinygiants.getalife.debug", appContext.packageName)
     }
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 buildscript {
-val kotlinVersion by extra("1.9.0")
+val kotlinVersion by extra("1.9.10")
 
     dependencies {
         classpath("com.google.gms:google-services:4.3.15")
         classpath("com.google.firebase:firebase-appdistribution-gradle:4.0.0")
-        classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.8")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.9")
         classpath("com.google.firebase:perf-plugin:1.4.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
     }
@@ -16,7 +16,7 @@ val kotlinVersion by extra("1.9.0")
 plugins {
     id("com.android.application") version "8.1.1" apply false
     id("com.android.library") version "8.1.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.21" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
     id("com.google.dagger.hilt.android") version "2.47" apply false
-    id("com.google.devtools.ksp") version "1.9.0-1.0.13" apply false
+    id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
 }
