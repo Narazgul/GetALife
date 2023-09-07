1f056195a2f6aebb043cc0f5cc319d2c972e3e7e
Michael Hofmann <michael.hofmann@stock3.com>
2023-09-08 00:45:05 +0200

    Use jvmToolchain

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,18 +64,15 @@ android {
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.3"
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+}
+
+kotlin {
+    jvmToolchain(17)
 }
 
 dependencies {
