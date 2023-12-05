428ac939b2ba3deb59d13fca614ccaa5bac67aee
Michael Hofmann <michael.hofmann@stock3.com>
2023-12-05 20:21:07 +0100

    Add crashlytics plugin on project level

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin) apply false
+    alias(libs.plugins.crashlytics) apply false
     alias(libs.plugins.hilt.plugin) apply false
     alias(libs.plugins.ksp) apply false
 }
