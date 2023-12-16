d894b14739975bf0c42a594295f30d4041fe7fa0
Michael Hofmann <michael.hofmann@stock3.com>
2023-12-16 21:04:39 +0100

    Prepare release Ausbrühten v0.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "app.tinygiants.getalife"
         minSdk = 28
         targetSdk = 34
-        versionCode = 1
-        versionName = "Ausbrühten 0.0.1"
+        versionCode = 2
+        versionName = "Ausbrühten 0.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
