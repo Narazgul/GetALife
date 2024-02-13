ed282578ab40b359aa58d7dcf6184cf468cceaa1
Michael Hofmann <michael.hofmann@stock3.com>
2024-02-13 21:30:20 +0100

    Change Font to default Google Sans Font

--- app/src/main/java/app/tinygiants/getalife/theme/Type.kt
+++ app/src/main/java/app/tinygiants/getalife/theme/Type.kt
@@ -10,100 +10,79 @@ import androidx.compose.material3.Typography
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import app.tinygiants.getalife.R
-
-val nunitoFontFamily = FontFamily(
-    Font(R.font.nunito),
-    Font(R.font.nunito_italic, style = FontStyle.Italic)
-)
 
 val Typography = Typography(
     displayLarge = TextStyle(
-        fontFamily = nunitoFontFamily,
         fontWeight = FontWeight.Light,
         fontSize = 101.sp,
         lineHeight = 64.sp,
         letterSpacing = (-1.5).sp
     ),
     displayMedium = TextStyle(
-        fontFamily = nunitoFontFamily,
         fontWeight = FontWeight.Light,
         fontSize = 45.sp,
         lineHeight = 52.sp,
         letterSpacing = (-0.5).sp
     ),
     displaySmall = TextStyle(
-        fontFamily = nunitoFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 36.sp,
         lineHeight = 44.sp,
         letterSpacing = 0.sp
     ),
     headlineLarge = TextStyle(
-        fontFamily = nunitoFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 32.sp,
         lineHeight = 40.sp,
         letterSpacing = 0.25.sp
     ),
     headlineMedium = TextStyle(
-        fontFamily = nunitoFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 28.sp,
         lineHeight = 36.sp,
         letterSpacing = 0.sp
     ),
     headlineSmall = TextStyle(
-        fontFamily = nunitoFontFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 24.sp,
         lineHeight = 32.sp,
         letterSpacing = 0.15.sp
     ),
     labelLarge = TextStyle(
-        fontFamily = nunitoFontFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 14.sp,
         lineHeight = 20.sp,
         letterSpacing = 0.1.sp
     ),
     labelMedium = TextStyle(
-        fontFamily = nunitoFontFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 12.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = nunitoFontFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 16.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     ),
     bodyLarge = TextStyle(
-        fontFamily = nunitoFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 14.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     ),
     bodyMedium = TextStyle(
-        fontFamily = nunitoFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 17.sp,
         lineHeight = 20.sp,
         letterSpacing = 0.25.sp
     ),
     bodySmall = TextStyle(
-        fontFamily = nunitoFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 12.sp,
         lineHeight = 16.sp,
@@ -111,7 +90,7 @@ val Typography = Typography(
     )
 )
 
-@Preview(widthDp = 600)
+@Preview(widthDp = 600, apiLevel = 33)
 @Composable
 fun TypographyPreview() {
     GetALifeTheme {
