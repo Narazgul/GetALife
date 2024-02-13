6f1f7904790202fa064c3c80584ae5c2ca9b6e7a
Michael Hofmann <michael.hofmann@stock3.com>
2024-02-13 21:12:37 +0100

    Change theme

--- app/src/main/java/app/tinygiants/getalife/theme/Theme.kt
+++ app/src/main/java/app/tinygiants/getalife/theme/Theme.kt
@@ -2,17 +2,13 @@ package app.tinygiants.getalife.theme
 
 import android.app.Activity
 import android.content.res.Configuration
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.WindowCompat
@@ -89,10 +85,10 @@ fun GetALifeTheme(
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
+//        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+//            val context = LocalContext.current
+//            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+//        }
         darkTheme -> darkColors
         else -> lightColors
     }
