058204394c1106c09da340409bb77a04af89340c
Michael Hofmann <michael.hofmann@stock3.com>
2024-02-18 13:29:30 +0100

    Remove api lvl 33 from previews

--- app/src/main/java/app/tinygiants/getalife/theme/Theme.kt
+++ app/src/main/java/app/tinygiants/getalife/theme/Theme.kt
@@ -4,11 +4,7 @@ import android.app.Activity
 import android.content.res.Configuration
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
@@ -115,6 +111,6 @@ fun GetALifeTheme(
     )
 }
 
-@Preview(name = "A-Light", widthDp = 400, apiLevel = 33)
-@Preview(name = "B-Dark", widthDp = 400, uiMode = Configuration.UI_MODE_NIGHT_YES, apiLevel = 33)
+@Preview(name = "A-Light", widthDp = 400)
+@Preview(name = "B-Dark", widthDp = 400, uiMode = Configuration.UI_MODE_NIGHT_YES)
 annotation class LightAndDarkPreviews
