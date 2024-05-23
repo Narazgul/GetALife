7ed3c975648bcbfffbc9ab63b1937d729021455c
Hofi <5161387+Narazgul@users.noreply.github.com>
2024-05-23 12:25:42 +0200

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "app.tinygiants.getalife"
         minSdk = 28
         targetSdk = 34
-        versionCode = 2
-        versionName = "Hatching 0.0.2"
+        versionCode = 3
+        versionName = "Hatching 0.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
